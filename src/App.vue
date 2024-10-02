232c34617d4914ef9a5ef9ece08a5b10dee5d725
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2024-10-02 17:43:37 +0200

    feat: add supense

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 
+
 </script>
 
 <template>
+  <RouterView v-slot="{ Component }">
+    <template v-if="Component">
+      <Transition mode="out-in">
+        <KeepAlive>
+          <Suspense>
+            <!-- main content -->
+            <component :is="Component"></component>
 
-
-
-  <RouterView />
+            <!-- loading state -->
+            <template #fallback>
+              Loading...
+            </template>
+          </Suspense>
+        </KeepAlive>
+      </Transition>
+    </template>
+  </RouterView>
 </template>
