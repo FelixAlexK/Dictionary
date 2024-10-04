270cc0885ab2afc9b9e7e15742770ee650781487
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2024-10-04 18:59:11 +0200

    chore: remove unused import

--- convex/entries.ts
+++ convex/entries.ts
@@ -1,7 +1,6 @@
 import { paginationOptsValidator } from 'convex/server'
 import { mutation, query } from './_generated/server'
 import { v } from 'convex/values'
-import { trace } from 'console'
 
 export const createEntry = mutation({
   args: {
